86046c246bd8b2fd52952d51dbe53b1fbafaec83
aware <aware9758@gmail.com>
2024-06-27 14:34:01 +0200

    search

--- components/AllTrips.tsx
+++ components/AllTrips.tsx
@@ -4,7 +4,13 @@ import supabase from "@/lib/supabase/supabase";
 import Image from "next/image";
 import Link from "next/link";
 import { useSwipeable } from "react-swipeable";
-import { compareDesc, differenceInDays, format, parseISO } from "date-fns";
+import {
+  compareAsc,
+  compareDesc,
+  differenceInDays,
+  format,
+  parseISO,
+} from "date-fns";
 import { MdTravelExplore } from "react-icons/md";
 import { GiAirplaneDeparture, GiAirplaneArrival } from "react-icons/gi";
 import { HiCurrencyRupee } from "react-icons/hi";
@@ -12,6 +18,8 @@ import { FaCalendarDays } from "react-icons/fa6";
 import { FcLikePlaceholder, FcLike, FcSms, FcShare } from "react-icons/fc";
 import { CiShare2 } from "react-icons/ci";
 import { PiChatCircleThin } from "react-icons/pi";
+import { CiSearch } from "react-icons/ci";
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
 
 function PostCard(post: Post) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -34,7 +42,7 @@ function PostCard(post: Post) {
 
   return (
     <div className="flex-none w-full sm:w-1/2 lg:w-1/2 p-2">
-      <div className="cursor-pointer p-2 sm:p-1.5 md:p-2 bg-custom-pri/5 rounded-lg">
+      <div className="cursor-pointer p-3 sm:p-4 md:p-6 bg-custom-pri/5 rounded-lg">
         <div
           className="relative border border-custom-pri border-opacity-30
          rounded-lg shadow-sm snap-always snap-center overflow-hidden"
@@ -69,14 +77,14 @@ function PostCard(post: Post) {
                   <div className="flex flex-col">
                     <time
                       dateTime={post.startDate}
-                      className="mb-1 text-lg lg:text-xl flex items-center gap-2"
+                      className="mb-1 text-base lg:text-lg flex items-center gap-2"
                     >
                       <GiAirplaneDeparture />
                       {format(parseISO(post.startDate), "LLLL d, yyyy")}
                     </time>
                     <time
                       dateTime={post.returnDate}
-                      className="mb-1 text-lg lg:text-xl flex items-center gap-2"
+                      className="mb-1 text-base lg:text-lg flex items-center gap-2"
                     >
                       <GiAirplaneArrival />
                       {format(parseISO(post.returnDate), "LLLL d, yyyy")}
@@ -87,12 +95,12 @@ function PostCard(post: Post) {
                   className="bg-custom-pri/45 backdrop-blur-lg drop-shadow-lg rounded-lg 
                 flex justify-between mr-auto px-4 py-2"
                 >
-                  <h3 className="text-2xl lg:text-3xl font-semibold mb-2 px-4 flex items-center gap-4">
+                  <h3 className="text-xl lg:text-2xl font-semibold mb-2 px-4 flex items-center gap-4">
                     <MdTravelExplore />
                     {post.title}
                   </h3>
                 </div>
-                <div className="mt-auto text-lg lg:text-xl">
+                <div className="mt-auto text-base lg:text-lg">
                   <p className="flex items-center gap-4">
                     <FaCalendarDays />
                     {differenceInDays(
@@ -110,7 +118,7 @@ function PostCard(post: Post) {
             </div>
           </div>
         </div>
-        <div className="flex justify-between mt-2">
+        <div className="flex justify-between mt-4 lg:mt-6">
           <div className="flex text-2xl space-x-2 py-1">
             <FcLikePlaceholder />
             <PiChatCircleThin className="text-custom-pri" />
@@ -133,6 +141,9 @@ function PostCard(post: Post) {
 function AllTrips() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [visiblePostsCount, setVisiblePostsCount] = useState(6);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
 
   useEffect(() => {
     const fetchTrips = async () => {
@@ -142,25 +153,39 @@ function AllTrips() {
       if (error) {
         console.error(error);
       } else {
-        setPosts(
-          data.map((trip) => ({
-            ...trip,
-            url: `/trips/${trip.id}`,
-            imageURL: trip.image, // Assuming this is an array of image URLs
-            startDate: trip.start_date,
-            returnDate: trip.return_date,
-            duration: trip.duration,
-            status: trip.status,
-            price: trip.price,
-            seats: trip.seats,
-          }))
+        const tripsWithDuration = data.map((trip) => ({
+          ...trip,
+          url: `/trips/${trip.id}`,
+          imageURL: trip.image, // Assuming this is an array of image URLs
+          startDate: trip.start_date,
+          returnDate: trip.return_date,
+          duration:
+            differenceInDays(
+              parseISO(trip.return_date),
+              parseISO(trip.start_date)
+            ) + 1,
+          status: trip.status,
+          price: trip.price,
+          seats: trip.seats,
+        }));
+
+        const sortedTrips = tripsWithDuration.sort((a, b) =>
+          sortOrder === "asc"
+            ? compareAsc(parseISO(a.startDate), parseISO(b.startDate))
+            : compareDesc(parseISO(a.startDate), parseISO(b.startDate))
         );
+
+        setPosts(sortedTrips);
       }
     };
 
     fetchTrips();
-  }, []);
-
+  }, [sortOrder]);
+  const filteredPosts = posts.filter((post) =>
+    Object.values(post).some((value) =>
+      value.toString().toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  );
   const showMorePosts = () => {
     setVisiblePostsCount((prevCount) => prevCount + 3);
   };
@@ -169,7 +194,7 @@ function AllTrips() {
     setVisiblePostsCount((prevCount) => Math.max(prevCount - 3, 3));
   };
   return (
-    <section className="max-w-6xl flex m-auto justify-between items-center mt-10">
+    <section className="max-w-6xl flex m-auto justify-between items-center mt-10 px-2">
       <div className="w-full pt-2">
         <h1
           className="h1 px-4
@@ -180,8 +205,28 @@ function AllTrips() {
         <a href="#travel-form" className="p-6 hover:cursor-pointer underline">
           Book with us -
         </a>
+        <div className="flex items-center justify-center p-4">
+          <div className="ml-auto flex items-center justify-center w-full max-w-xs relative text-custom-pri">
+            <CiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 " />
+            <input
+              type="text"
+              className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:border-custom-pri"
+              placeholder="Search trips..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+          <button
+            className="ml-auto border rounded-full font-semibold text-lg px-2 py-2
+          bg-white hover:border-custom-pri hover:text-custom-pri hover:shadow-xl 
+          transition duration-300"
+            onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}
+          >
+            <HiOutlineAdjustmentsHorizontal className="text-xl" />
+          </button>
+        </div>
         <div className="flex flex-wrap justify-between items-center">
-          {posts.slice(0, visiblePostsCount).map((post, idx) => (
+          {filteredPosts.slice(0, visiblePostsCount).map((post, idx) => (
             <PostCard key={idx} {...post} />
           ))}
         </div>
