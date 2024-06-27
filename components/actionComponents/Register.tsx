4dbc7533380b60a4b66fd911878da8bd170cb721
Bablu202 <102294921+Bablu202@users.noreply.github.com>
2024-06-27 15:55:32 +0200

    Update Register.tsx

--- components/actionComponents/Register.tsx
+++ components/actionComponents/Register.tsx
@@ -100,7 +100,7 @@ export const RegisterForm = () => {
       </div>
       <button
         type="submit"
-        style={{ backgroundColor: `${isPending ? "#0D0D0D" : "#033E8C"}` }}
+        style={{ backgroundColor: `${isPending ? "#0D0D0D" : "#24386E"}` }}
         className="inline-block px-7 py-2 bg-custom-pri text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
         disabled={isPending}
       >
