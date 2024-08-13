e8616c7a4a8243db93185547c48eee8ca590d5e6
aware <aware9758@gmail.com>
2024-08-13 08:08:03 +0200

    undo

--- components/dashboardComponents/AnalyticsManage.tsx
+++ components/dashboardComponents/AnalyticsManage.tsx
@@ -1,36 +1,13 @@
-// pages/analytics.tsx
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import ChartsComponent from "./ChartComponent";
-import { TripUser } from "@/types/custom";
+// components/AnalyticsManage.tsx
+import React from "react";
 
-const AnalyticsPage: React.FC = () => {
-  const [trips, setTrips] = useState<
-    { trip_name: string; users: TripUser[] }[]
-  >([]);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchTrips = async () => {
-      try {
-        const response = await axios.get("/api/user/manage/get-trips");
-        setTrips(response.data.trips);
-      } catch (err) {
-        setError("Failed to fetch trips data");
-      }
-    };
-
-    fetchTrips();
-  }, []);
-
-  if (error) return <p className="text-red-500">{error}</p>;
-
+const AnalyticsManage: React.FC = () => {
   return (
     <div className="container mt-12 mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Analytics Dashboard</h1>
-      <ChartsComponent trips={trips} />
+      <p>View analytics here...</p>
     </div>
   );
 };
 
-export default AnalyticsPage;
+export default AnalyticsManage;
