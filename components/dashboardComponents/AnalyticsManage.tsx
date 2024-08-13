3413f6bef382eadffb23a63799784cb434fd761f
aware <aware9758@gmail.com>
2024-08-13 09:27:27 +0200

    charts

--- components/dashboardComponents/AnalyticsManage.tsx
+++ components/dashboardComponents/AnalyticsManage.tsx
@@ -5,7 +5,6 @@ const AnalyticsManage: React.FC = () => {
   return (
     <div className="container mt-12 mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Analytics Dashboard</h1>
-      <p>View analytics here...</p>
     </div>
   );
 };
